401aa7656a69f0bfee37c521855d185b454e5055
Гунарді Сутанто <lonecalvary78.de@gmail.com>
2024-08-02 18:45:54 +0800

    Amend Book Renting DTO

--- src/main/java/demo/app/book/domain/rent/model/RentRequestDTO.java
+++ src/main/java/demo/app/book/domain/rent/model/RentRequestDTO.java
@@ -1,22 +1,21 @@
 package demo.app.book.domain.rent.model;
 
 import jakarta.json.bind.annotation.JsonbDateFormat;
-import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
 
-@RequiredArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
+@NotNull
 public class RentRequestDTO {
-  @Min(1)
   private Long bookId;
-  @Min(1)
   private Long borrowerId;
-
   @JsonbDateFormat("yyyy-MM-dd")
   private LocalDate fromDate;
   @JsonbDateFormat("yyyy-MM-dd")
